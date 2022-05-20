d3c8098ac98fb9685e4ab2eb921769a85c2b6476
Sanusi David <sanusidavid360@gmail.com>
2022-05-20 18:25:19 +0100

    api

--- note_project/urls.py
+++ note_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('note_app.urls'))
+    path('tasks/', include('note_app.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
